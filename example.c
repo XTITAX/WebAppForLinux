b3ebc53bf9a3234921d3d1e50cf9c3e9552cb1b2
XTITAX <xtitax@yandex.ru>
2024-07-22 15:39:22 +0300

    Update example.c

--- example.c
+++ example.c
@@ -42,6 +42,9 @@ int main(int argc, char *argv[]) {
     gtk_window_set_title(GTK_WINDOW(window), "Калькулятор суммы");
     g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
 
+    // Устанавливаем размеры окна (в два раза больше)
+    gtk_window_set_default_size(GTK_WINDOW(window), 400, 300);
+
     // Создаем сетку для размещения элементов
     grid = gtk_grid_new();
     gtk_container_add(GTK_CONTAINER(window), grid);
